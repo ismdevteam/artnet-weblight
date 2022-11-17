5363bc0b92e3ad9cd4b5cdf8f875fa12978b22a8
ismdevteam <dajenedumai3@gmail.com>
2022-11-18 02:35:10 +0300

    update

--- weblight.js
+++ weblight.js
@@ -9,6 +9,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const glob = require("glob");
 
+const fs = require('fs');
+const path = require('path');
+
 
 const config = {};
 config.artnet = {};
@@ -51,8 +54,15 @@ const srv = artnet.listen(config.artnet.port, function (msg, peer) {
 
             buffer[index] = value;
 
-            let deviceId = ((index - 5) / 10).toFixed(0);
-            if (deviceId < 1) deviceId = 0;
+            let deviceId = 0;
+            if (index === 30) {
+                        updateIMGA(deviceId);
+                        return;
+            }
+            if (index === 60) {
+                        updateIMGB(deviceId);
+                        return;
+            }
             updateClient(deviceId);
         });
 });
@@ -142,8 +152,76 @@ glob("images/*", {}, function (er, files) {
     imageFiles = files;
 });
 
+let textByLine = [128];
+fs.readFile('config/0/subtitles.conf', 'utf8', function(err, data){
+    textByLine = data.split("\n");
+    textByLine.unshift("none");
+});
+
+let layer1imagePath = [128];
+fs.readFile('config/0/layer.conf', 'utf8', function(err, data){
+    layer1imagePath = data.split("\n");
+    layer1imagePath.unshift('none');
+});
+
+let layer2imagePath = [128];
+fs.readFile('config/0/layer.conf', 'utf8', function(err, data){
+    layer2imagePath = data.split("\n");
+    layer2imagePath.unshift('none');
+});
+
+function updateIMGA(deviceId) {
+    const layerAsrc = (buffer[deviceId + 30] / 2).toFixed(0); //layer1 img src   
+    let imagePath1 = "none";
+    if (layer1imagePath[layerAsrc] === undefined ) imagePath1 = "none";
+     else imagePath1 = path.join('images', layer1imagePath[layerAsrc]); 
+    if (!imagePath1) imagePath1 = "images/none";    
+   io.sockets.emit('imga', {layer1_image: imagePath1});
+}
+
+function updateIMGB(deviceId) {
+    const layerBsrc = (buffer[deviceId + 60] / 2).toFixed(0); //layer2 img src
+    let imagePath2 = "none";
+    if (layer2imagePath[layerBsrc] === undefined ) imagePath2 = "none";
+     else imagePath2 = path.join('images', layer2imagePath[layerBsrc]); 
+    if (!imagePath2) imagePath2 = "images/none";
+   io.sockets.emit('imgb', {	layer2_image: imagePath2});
+}
+
 
 function updateClient(deviceId) {
+    //const deviceNr = deviceId * 10;
+    const deviceNr = deviceId;
+    
+    const titlesSelector = (buffer[deviceNr + 6] / 2).toFixed(0);
+
+    io.sockets.emit('dev' + deviceId, {
+    	index: titlesSelector,
+        a: (buffer[deviceNr + 0] / 255).toFixed(3),
+        r: buffer[deviceNr + 1],
+        g: buffer[deviceNr + 2],
+        b: buffer[deviceNr + 3],
+	panel_opacity: (buffer[deviceNr + 10] / 255).toFixed(3), 
+	//text: textByLine[titlesSelector],
+	subtitles_a: (buffer[deviceNr + 10] / 255).toFixed(3),
+        subtitles_r: buffer[deviceNr + 7],
+        subtitles_g: buffer[deviceNr + 8],
+        subtitles_b: buffer[deviceNr + 9],
+        subtitles_rotate: (buffer[deviceNr + 11] * 1.41176470588 + 179).toFixed(0),
+        subtitles_zoom: (buffer[deviceNr + 12] / 255 * 2).toFixed(3),
+	//layer1_image: imagePath1,
+	layer1_opacity: (buffer[deviceNr + 36] / 255).toFixed(3),
+        layer1_rotate: (buffer[deviceNr + 37] * 1.41176470588 + 179).toFixed(0),
+        layer1_zoom: (buffer[deviceNr + 38] / 255 * 2).toFixed(3),
+	//layer2_image: imagePath2,
+	layer2_opacity: (buffer[deviceNr + 66] / 255).toFixed(3),
+	layer2_rotate: (buffer[deviceNr + 67] * 1.41176470588 + 179).toFixed(0),
+        layer2_zoom: (buffer[deviceNr + 68] / 255 * 2).toFixed(3),
+    });
+}
+
+
+function updateClient_bkp(deviceId) {
     const deviceNr = deviceId * 10;
 
     const imageNumber = (buffer[deviceNr + 6] / 10).toFixed(0);
